a424fa395e9fc43d2061e3f37dd3120b018785dc
Jaime <jaime.lapiz@usjr.edu.ph>
2022-10-15 10:20:56 +0800

    added Ticket entity

--- src/main/java/ph/com/group6/ticketModule/entity/Ticket.java
+++ src/main/java/ph/com/group6/ticketModule/entity/Ticket.java
@@ -1,5 +1,48 @@
 package ph.com.group6.ticketModule.entity;
 
 public class Ticket {
-
+	private int ticketID;
+	private String assignee;
+	private String status;
+	private String subject;
+	private String description;
+	private String tracker;
+	
+	public int getTicketID() {
+		return ticketID;
+	}
+	public void setTicketID(int ticketID) {
+		this.ticketID = ticketID;
+	}
+	public String getAssignee() {
+		return assignee;
+	}
+	public void setAssignee(String assignee) {
+		this.assignee = assignee;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getTracker() {
+		return tracker;
+	}
+	public void setTracker(String tracker) {
+		this.tracker = tracker;
+	}
+	
 }
