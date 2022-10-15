6ccb9c814ddea723dcb4222ed657256105ab2002
Jaime <jaime.lapiz@usjr.edu.ph>
2022-10-15 10:07:47 +0800

    initial commit

---
+++ src/main/java/ph/com/group6/ticketModule/TicketModuleApplication.java
@@ -0,0 +1,13 @@
+package ph.com.group6.ticketModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketModuleApplication.class, args);
+	}
+
+}
